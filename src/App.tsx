435bd155cd55c2bcb0513718c8260426ee718962
gracedemos <graysondemos@gmail.com>
2023-08-30 13:35:05 -0400

    CSS improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 
 import Messages from "./Messages";
 import MessageBar from "./MessageBar";
 
 function App() {
 	return (
-		<div id="main">
+		<div id="chat">
 			<Messages/>
 			<MessageBar name="Juan"/>
 		</div>
